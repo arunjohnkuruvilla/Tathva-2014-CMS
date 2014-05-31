19fdafe5a727af236882f8c8cb116aaa11f270cf
noonetoogreat <arunjohnkuruvilla@gmail.com>
2014-05-31 16:17:24 +0530

    fix the table of users in terminal.php

--- modules/table-users.php
+++ modules/table-users.php
@@ -6,19 +6,34 @@ if (!$row)
     echo "Waiting for first contact!";
   else 
   {
-    echo "
-      <table>USERS
-        <thead>
-          <tr>
-            <th>ROLLNUMBER</th>
-            <th>Username</th> 
-            <th>Password</th> 
-            <th>Eventcode</th> 
-            <th>Validation</th> 
-            <th>Delete</th> 
-            <th>Validation Status</th> 
-          </tr>
-        </thead>";
+    echo '
+      <style type="text/css">
+        .tg  {border-collapse:collapse;border-spacing:0;}
+        .tg td{font-family:Arial, sans-serif;font-size:14px;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;}
+        .tg th{font-family:Arial, sans-serif;font-size:14px;font-weight:normal;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;}
+        .tg .tg-glis{font-size:10px}
+      </style>
+      <table class="tg" style="undefined;table-layout: fixed; width: 925px">
+      <colgroup>
+        <col style="width: 152px">
+          <col style="width: 152px">
+          <col style="width: 152px">
+          <col style="width: 102px">
+          <col style="width: 102px">
+          <col style="width: 102px">
+          <col style="width: 163px">
+      </colgroup>
+      <thead>
+        <tr>
+          <th>ROLLNUMBER</th>
+          <th>Username</th> 
+          <th>Password</th> 
+          <th>Eventcode</th> 
+          <th>Validation</th> 
+          <th>Delete</th> 
+          <th>Validation Status</th> 
+        </tr>
+      </thead>';
     do {
   $u = $row['username'];
   $x = "exec.php?u=$u";
@@ -26,15 +41,15 @@ if (!$row)
   $v .= ($row['validate'] == 0) ? "val'>Validate" : "inv'>Invalidate";
   $v .= "</a>";
   echo "
-    <tr>
-      <td>$row[roll]</td>
-      <td>$u</td> 
-      <td>$row[password]</td> 
-      <td>$row[eventcode]</td> 
-      <td>$v</td> 
-      <td><a href='$x&a=del'>Delete</a></td>
-      <td style='text-align:center'>$row[validate]</td>
-    </tr>";
+  <tr>
+    <th class='tg-glis'>$row[roll]</th>
+    <th class='tg-glis'>$u</th>
+    <th class='tg-glis'>$row[password]</th>
+    <th class='tg-glis'>$row[eventcode]</th>
+    <th class='tg-glis'>$v</th>
+    <th class='tg-glis'><a href='$x&a=del'>Delete</a></th>
+    <th class='tg-glis' style='text-align:center'>$row[validate]</th>
+  </tr>";
     } while($row=$result->fetch_array());
   }
     ?></table>
